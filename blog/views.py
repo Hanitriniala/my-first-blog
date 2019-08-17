587e3781d5f8ffa244caf716707c5068f9246383
Hanitriniala <sitraka.rasolohery@gmail.com>
2019-08-17 16:47:19 +0300

    commit avec site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, Http404
+
+def sitraka(request):
+
+    return HttpResponse("""
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>sitraka</title>:
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  ...
+  <!-- Le reste du contenu -->
+  <H1 style ="color:red" > Blog de Sitraka  </H1> 
+
+  <p> Bla bla bla </p>
+  ...
+</body>
+</html>
+    """)
+
+
+
+
+
+
+
+    # HTML
